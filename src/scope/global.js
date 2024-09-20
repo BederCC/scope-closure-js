fc5d4b8fb17c16fd7c23e48ddb798870c815ef02
BederCC <beder1810@hmail.com>
2024-09-20 00:01:00 -0500

    global.js terminado

--- src/scope/global.js
+++ src/scope/global.js
@@ -1,1 +1,23 @@
 // variables
+
+var a; // declarando la variable
+var b = 2; // declarando y asignamos o inicializamos la variable
+b = 3; // reasignando la variable
+var a = 30; // redeclarando la variable
+
+// Global scope
+var fruit = 'apple'; // global scope
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit(); // apple
+
+function countries() {
+    country = 'Peru'; // al no poner var, let o const se convierte en una variable global
+    console.log(country);
+}
+
+countries(); // Peru
+console.log(country); // Peru
